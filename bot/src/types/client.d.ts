a80cd7d56d58756d3acf0c04d548ee825c79a5f0
kystrel <25458428+kystrel@users.noreply.github.com>
2026-07-29 20:37:50 -0500

    chore: align workspace configuration (#389)

--- bot/src/types/client.d.ts
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { Client, Collection } from 'discord.js';
-
-declare module 'discord.js' {
-    export interface Client {
-        commands: Collection<string, SlashCommandBuilder>;
-        execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
-    }
-}
